9e4ed43620d9590f8a624ab44e02e4332e782861
munrexio <munrexio@gmail.com>
2020-05-15 05:05:39 +0300

    bugfix
    Переделал схему: добавил проверку полей, буквы заменил человекочитаемыми опциями, добавил опцию для дополнительных интерфейсов. Переделал скрипт. Убрал лишние файлы. knxd.service больше не трогаем: добавил knxd в dialout, сокет оставляем. Добавил предупреждение в схему по этому поводу ( о том что не нужно использовать опции -i и -u. )

--- knxd-patch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-service knxd stop
-rm -f /lib/systemd/system/knxd.socket
-cp -f /usr/lib/wb-knxd-config/knxd.service /lib/systemd/system/knxd.service
-systemctl daemon-reload && service knxd start
--- wb-knxd-config.sh
+++ wb-knxd-config.sh
@@ -1,119 +1,86 @@
 #!/bin/bash
 json1=$(jq '' /etc/wb-knxd-config.conf)
-f=$(echo $json1 | jq '.global.f' | sed -e 's/\"//g')
-t=$(echo $json1 | jq '.global.t' | sed -e 's/\"//g')
-c=$(echo $json1 | jq '.cache.c' | sed -e 's/\"//g')
-e=$(echo $json1 | jq '.address.e' | sed -e 's/\"//g')
-E=$(echo $json1 | jq '.address.E' | sed -e 's/\"//g')
-b=$(echo $json1 | jq '.interface.b' | sed -e 's/\"//g')
-i=$(echo $json1 | jq '.localListener.i' | sed -e 's/\"//g')
-u=$(echo $json1 | jq '.localListener.u' | sed -e 's/\"//g')
-D=$(echo $json1 | jq '.multicastServer.D' | sed -e 's/\"//g')
-T=$(echo $json1 | jq '.multicastServer.T' | sed -e 's/\"//g')
-R=$(echo $json1 | jq '.multicastServer.R' | sed -e 's/\"//g')
-S=$(echo $json1 | jq '.multicastServer.S' | sed -e 's/\"//g')
-n=$(echo $json1 | jq '.multicastServer.n' | sed -e 's/\"//g')
-forced=$(echo $json1 | jq '.multicastServer.forced' | sed -e 's/\"//g')
-name=$(echo $json1 | jq '.multicastServer.name'| sed -e 's/\"//g')
-Server=$(echo $json1 | jq '.multicastServer.Server'| sed -e 's/\"//g')
-if [ "$f" = "null" ]
-then
-	f=""
+error=$(echo $json1 | jq '.Global.Error' | sed -e 's/\"//g') 					# "null" or "SomeText" ok
+trace=$(echo $json1 | jq '.Global.Trace' | sed -e 's/\"//g') 					# "null" or "SomeText" ok
+listenTcp=$(echo $json1 | jq '.LocalListener.ListenTcp' | sed -e 's/\"//g') 	# "null" or "SomeText" ok
+listenLocal=$(echo $json1 | jq '.LocalListener.ListenLocal' | sed -e 's/\"//g') # "null" or "SomeText" ok
+otherOptions=$(echo $json1 | jq '.Interface.OtherOptions' | sed -e 's/\"//g')   # "" or "SomeText"
+name=$(echo $json1 | jq '.MulticastServer.Name'| sed -e 's/\"//g')  			# "" or "SomeText" ok
+eibAddr=$(echo $json1 | jq '.Address.EibAddr' | sed -e 's/\"//g') 				# "SomeText" ok
+clientAddrs=$(echo $json1 | jq '.Address.ClientAddrs' | sed -e 's/\"//g') 		# "SomeText" ok
+layer2=$(echo $json1 | jq '.Interface.Layer2' | sed -e 's/\"//g') 				# "SomeText"
+address=$(echo $json1 | jq '.MulticastServer.Address'| sed -e 's/\"//g') 		# "SomeText" ok
+groupCache=$(echo $json1 | jq '.Cache.GroupCache' | sed -e 's/\"//g') 			# true/false/"null" ok
+discovery=$(echo $json1 | jq '.MulticastServer.Discovery' | sed -e 's/\"//g') 	# true/false ok
+tunnelling=$(echo $json1 | jq '.MulticastServer.Tunnelling' | sed -e 's/\"//g') # true/false ok
+routing=$(echo $json1 | jq '.MulticastServer.Routing' | sed -e 's/\"//g') 		# true/false ok
+server=$(echo $json1 | jq '.MulticastServer.Server' | sed -e 's/\"//g') 		# true/false ok
+forced=$(echo $json1 | jq '.MulticastServer.Forced' | sed -e 's/\"//g') 		# true/false ok
+
+if [[ "$error" = "null" ]]; then
+	error=""
 else
-	f="-f$f "
+	error="-f$error"
 fi
-if [ "$t" = "null" ]
-then
-	t=""
+
+if [[ "$trace" = "null" ]]; then
+	trace=""
 else
-	t="-t$t "
+	trace="-t$trace "
 fi
-if [ "$e" = "" ]
-then
-	e="-e 0.0.1 "
+
+if [[ "$listenTcp" = "null" ]]; then
+	listenTcp=""
 else
-	e="-e $e "
+	listenTcp="-i $listenTcp "
 fi
-if [ "$E" = "" ]
-then
-	E="-E 0.0.2:50 "
+
+if [[ "$listenLocal" = "null" ]]; then
+	listenLocal=""
 else
-	E="-E $E "
+	listenLocal="-u $listenLocal "
 fi
-if [ "$c" = "true" ]
-then
-	c="-c "
+
+if [[ "$groupCache" = "true" ]]; then
+	groupCache="-c "
 else
-	c=""
+	groupCache=""
 fi
-if [ "$forced" = "true" ]
-then
-	forced="--allow-forced-broadcast "
+
+if [[ "$server" = "true" ]]; then
+	if [[ "$name" = "" ]]; then
+		server="-S $address "
+	else
+		server="-n $name -S $address "
+	fi
+	if [[ "$discovery" = "true" ]]; then
+		discovery="-D "
+	else
+		discovery=""
+	fi
+	if [[ "$tunnelling" = "true" ]]; then
+		tunnelling="-T "
+	else
+		tunnelling=""
+	fi
+	if [[ "$routing" = "true" ]]; then
+		routing="-R "
+	else
+		routing=""
+	fi
+	if [[ "$forced" = "true" ]]; then
+		forced="--allow-forced-broadcast "
+	else
+		forced=""
+	fi
 else
 	forced=""
+	discovery=""
+	tunnelling=""
+	routing=""
+	server=""
 fi
-if [ "$D" = "true" ]
-then
-	D="-D "
-else
-	D=""
-fi
-if [ "$T" = "true" ]
-then
-	T="-T "
-else
-	T=""
-fi
-if [ "$R" = "true" ]
-then
-	R="-R "
-else
-	R=""
-fi
-if [ "$n" = "true" ]
-then
-	if [ "$name" = "" ]
-	then
-		name="knxd"
-	fi
-	n="-n '$name' "
-else
-	n=""
-fi
-if [ "$S" = "true" ]
-then
-	if [ "$Server" = "" ]
-	then
-		Server="224.0.23.12:3671"
-	fi
-	S="-S $Server "
-else
-	S=""
-fi
-if [ "$i" = "" ]
-then
-	i="-i 6720 "
-elif [ "$i" = "null" ]
-then
-	i=""
-else
-	i="-i $i "
-fi
-if [ "$u" = "" ]
-then
-	u="-u /var/run/knx "
-elif [ "$u" = "null" ]
-then
-	u=""	
-else
-	u="-u $u "
-fi
-if [ "$b" = "" ]
-then
-	b="-b ncn5120:/dev/ttyMOD1"
-else
-	b="-b $b"
-fi
+
 #knxd [global-section] [address-section] [cache-section] [multicast-server-section] [local-listener-section] [interface-sections]
-echo "KNXD_OPTS=\"$t$f$e$E$c$forced$D$T$R$n$S$i$u$b\"" > /etc/knxd.conf
+echo "KNXD_OPTS=\"$trace$error -e $eibAddr -E $clientAddrs $groupCache$forced$discovery$tunnelling$routing$server$listenTcp$listenLocal -b $layer2 $otherOptions\"" > /etc/knxd.conf
 service knxd restart
